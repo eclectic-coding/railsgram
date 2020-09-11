ce312d9b4a35f40518f9070317f5096a6ddf303d
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-09-11 12:22:03 -0400

    Add custom stylesheet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,5 +15,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "../stylesheets/application.scss"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
